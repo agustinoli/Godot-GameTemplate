27b0fd2430fd94e6ca597b3d5a9b589aa3c14a68
Tomas Sarquis <tomas.sarquis@wazuh.com>
2022-08-28 22:16:00 -0300

    IMprove credits generation

--- addons/GameTemplate/CreditsScreen/CreditsScreen.gd
+++ addons/GameTemplate/CreditsScreen/CreditsScreen.gd
@@ -1,20 +1,15 @@
 extends Node2D
 
 func _ready():
-	var config = ConfigFile.new()
-	var err = config.load("res://credits.cfg")
-	var amount
+	$CanvasLayer/Label.text = tr("CREDITS")
+	var more_credits = true
+	var credit_index = 1
 	
-	if err != OK:
-		amount = 0
-	else:
-		amount = config.get_value("credits", "amount")
-	
-	var title = tr("CREDITS")
-	var credits = ""
-	
-	for n in amount:
-		credits += str( tr( str("CREDITS",n+1) ), " ", tr("BY"), "\n" )
-	
-	$CanvasLayer/Label.text = str(title, "\n\n", credits)
-	
+	$CanvasLayer/Label.text = "CREDITS\n"
+	while more_credits:
+		var credit_sr = str("CREDITS", credit_index)
+		if tr(credit_sr) == credit_sr:
+			more_credits = false
+		else:
+			$CanvasLayer/Label.text += str( tr(credit_sr), " ", tr("BY"), "\n" )
+			credit_index += 1
